3f813a7d2cc34e87401c7e7b9100140e34df46a6
Daniel Ramon Campos <drc89.sxm@gmail.com>
2024-01-30 11:16:15 +0100

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../modules/user.model.js"
+import bcryptjs from 'bcryptjs'
+
+/* Crea un usuario en la base de datos */
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body //obtenemos los datos desde el body
+    const hashedPassword = bcryptjs.hashSync(password, 10) // encriptamos la contraseña
+    const newUser = new User({username, email, password: hashedPassword})
+    try{
+        await newUser.save()
+        res.status(201).json({message: "User creeated successfully"})
+    }catch(error){
+        res.status(500).json(error.message)
+    }
+}
--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+/* Instalamos nodemon para no tener que parar y arrancar el servidor en cada cambio que hagamos */
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config() //leer la configuración del archivo .env
 /* Conexión a la base de datos */
@@ -14,6 +16,8 @@ mongoose.connect(process.env.MONGO) // datos de la base de datos en el archivo .
 })
 
 const app = express() // Inicializamos el servidor
+app.use(express.json()) //Permite que JSON sea la entrada (de datos) de nuestro backend
+
 
 /* Asignamos un puerto al servidor */
 app.listen(3000, ()=>{
@@ -22,3 +26,4 @@ app.listen(3000, ()=>{
 
 /* endPoint */
 app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes)

fd7af11b3d89fac7a957a5765c3dd44e0910c209
Daniel Ramon Campos <drc89.sxm@gmail.com>
2024-02-03 13:47:15 +0100

    complete update user functionality

--- client/src/components/oAuth.jsx
+++ client/src/components/oAuth.jsx
@@ -2,10 +2,12 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
 import { app } from '../firebase'
 import { useDispatch } from 'react-redux'
 import { signInSuccess } from '../redux/user/userSlice.js'
+import { useNavigate } from 'react-router-dom'
 
 export default function OAuth() {
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const handleGoogleClick = async () => {
         try{
@@ -25,6 +27,7 @@ export default function OAuth() {
             })
             const data = await res.json()
             dispatch(signInSuccess(data))
+            navigate('/')
         }catch (error) {
             console.log('Could not login with Google', error)
         }

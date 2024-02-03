deb3f621b55fd104b912ca9dc34532b6dfb97a04
Daniel Ramon Campos <drc89.sxm@gmail.com>
2024-02-03 18:04:30 +0100

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 dotenv.config() //leer la configuración del archivo .env
 /* Conexión a la base de datos */
@@ -16,7 +17,15 @@ mongoose.connect(process.env.MONGO) // datos de la base de datos en el archivo .
     console.log(err)
 })
 
+const __dirname = path.resolve() //Producción
+
 const app = express() // Inicializamos el servidor
+
+app.use(express.static(path.join(__dirname, '/client/dist'))) //Producción
+app.get('*', (req, res) => { //Producción
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))//Producción
+})
+
 app.use(express.json()) //Permite que JSON sea la entrada (de datos) de nuestro backend
 app.use(cookieParser())
 
